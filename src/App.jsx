a195f162b89c45b1bb458ad7eeab546aec8b3136
Lina Martinez <lmcastro3501@gmail.com>
2025-06-13 23:14:13 -0700

    Add total likes counter

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import ChatLog from './components/ChatLog';
 import DATA from './data/messages.json';
 import { useState } from 'react';
 
-
 const App = () => {
   const [entryData, setEntryData] = useState(DATA);
   // const firstMsgData = DATA[0];
@@ -21,13 +20,29 @@ const App = () => {
     });
   };
 
+  //////////////////////////
+  // CALCULATE TOTAL LIKES//
+  /////////////////////////
+  // let totalLikes = 0;
+  // for (const entry of entryData) {
+  //   if (entry.liked) {
+  //     totalLikes += 1;
+  //   }
+  // }
+  const totalLikes = entryData.reduce((sum, entry) => {
+    return entry.liked ? sum + 1 : sum;
+  }, 0);
+
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat Between {entryData[0].sender} and {entryData[1].sender} </h1>
+        <section>
+          <span id='heartWidget' className='widget'>{totalLikes} ❤️s</span>
+        </section>
       </header>
       <main>
-        {/* Display Single Msg - Wave 1
+        {/* Display Single Msg - WAVE 1
         <ChatEntry
           sender={firstMsgData.sender}
           body={firstMsgData.body}
