ed72cdea48e5d8e0a165900efa2e64cdc9b9d420
Lina Martinez <lmcastro3501@gmail.com>
2025-06-13 21:58:49 -0700

    Implement event handler to invoke onToggleLike() when an entry is liked.

--- src/components/ChatEntry.jsx
+++ src/components/ChatEntry.jsx
@@ -2,23 +2,31 @@ import './ChatEntry.css';
 import TimeStamp from './TimeStamp';
 import PropTypes from 'prop-types';
 
-const ChatEntry = ({sender, body, timeStamp}) => {
+const ChatEntry = ({id, sender, body, timeStamp, liked, onToggleLike}) => {
   return (
     <div className="chat-entry local">
       <h2 className="entry-name">{sender}</h2>
       <section className="entry-bubble">
         <p>{body}</p>
         <p className="entry-time"><TimeStamp time={timeStamp}/></p>
-        <button className="like">🤍</button>
+        <button
+          className="like"
+          onClick={() => {onToggleLike(id);}}
+        >
+          {liked ? '❤️': '🤍'}
+        </button>
       </section>
     </div>
   );
 };
 
 ChatEntry.propTypes = {
+  id: PropTypes.number.isRequired,
   sender: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
-  timeStamp: PropTypes.string.isRequired
+  timeStamp: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
+  onToggleLike: PropTypes.func.isRequired,
 };
 
 export default ChatEntry;
