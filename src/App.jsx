b5626bd26e4151a6eeead0bce327f10fc01f8028
Lina Martinez <lmcastro3501@gmail.com>
2025-06-13 20:47:23 -0700

    Update the App component to display an entire chat log and comment single chat entry.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
-import ChatEntry from './components/ChatEntry';
+// import ChatEntry from './components/ChatEntry';
+import ChatLog from './components/ChatLog';
 import messagesData from './data/messages.json';
 
 
 const App = () => {
-  const firstMsgData = messagesData[0];
+  // const firstMsgData = messagesData[0];
 
   return (
     <div id="App">
@@ -12,11 +13,14 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
+        {/* Display Single Msg - Wave 1
         <ChatEntry
           sender={firstMsgData.sender}
           body={firstMsgData.body}
           timeStamp={firstMsgData.timeStamp}>
         </ChatEntry>
+        */}
+        <ChatLog entries={messagesData} />
       </main>
     </div>
   );
